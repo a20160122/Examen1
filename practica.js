df8561421292f7c8f95daa8442024789d052e450
a20160122 <131172742+a20160122@users.noreply.github.com>
2023-09-26 08:46:52 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,16 @@
+alert("Bienvenido al Examen");
+var nom = prompt("Ingrese su nombre y apellido");
+var res1 = ("¿Cuándo se realizará la inauguración de las Olimpiadas?\nA:Lunes 02 de octubre\nB:Viernes 29 de setiembre\nC:Jueves 28 de setiembre");
+if(res1=="B"){
+    res1 = 0;
+}else{
+    res1=0;
+}
+
+var res2 = ("¿De qué color es la prompoción de 2do de secundaria?\nA:Celeste o Turquesa\nB:Rojo\nC:Morado");
+alert("Bienvenido a mi examen");
+if(res1=="A"){
+    res1 = 0;
+}else{
+    res1=0;
+}
